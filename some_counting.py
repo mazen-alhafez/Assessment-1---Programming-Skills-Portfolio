472e68202fe30dcfbc91c15058241224741bba05
mazen-alhafez <mazen.4lhafez@outlook.com>
2025-12-29 09:03:30 +0300

    Add files via upload

---
+++ some_counting.py
@@ -0,0 +1,27 @@
+# for loop to count from 0 to 50 by adding 1
+for i in range(0, 51):
+    print(i)
+#just used empty print to make the output easy to read
+print()
+print()
+# counting down from 50 to 0
+for i in range(50, -1, -1):
+    print(i)
+
+print()
+print()
+#count from 30 to 50
+for i in range(30, 51):
+    print(i)
+
+print()
+print()
+#counting from 50 to 10 -2
+for i in range(50, 9, -2):
+    print(i)
+
+print()
+print()
+#counting up from 100 to 200 by adding 5
+for i in range(100, 201, 5):
+    print(i)

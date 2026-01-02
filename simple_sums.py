8bd8fdadb53f19eea3bff6f549ecc72be46ae5e6
mazen-alhafez <mazen.4lhafez@outlook.com>
2026-01-02 14:12:57 +0300

    Delete simple_sums.py

--- simple_sums.py
+++
@@ -1,8 +0,0 @@
-#in this exercise i declared two variables initialized each with an integer and created a third variable to store their sum.
-#the second part of the code is printing the sum of these two numbers which is 18.
-
-num8 = 8
-num10 = 10
-sum = num8 + num10
-
-print(sum)

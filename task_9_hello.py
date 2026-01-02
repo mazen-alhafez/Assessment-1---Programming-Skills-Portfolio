285247bf5c8b09789ef470759d2a4d09484c1b9b
mazen-alhafez <mazen.4lhafez@outlook.com>
2026-01-02 14:21:43 +0300

    Add files via upload

---
+++ task_9_hello.py
@@ -0,0 +1,9 @@
+#def function called hello and printed hello in the output
+def hello():
+    print("Hello")  
+#i ran the hello function
+def main():
+    hello()
+#to start the program 
+if __name__ == "__main__":
+    main()

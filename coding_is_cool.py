3c1524083c6680d900780b67a8936f3e5444dd06
mazen-alhafez <mazen.4lhafez@outlook.com>
2026-01-02 14:11:18 +0300

    Delete coding_is_cool.py

--- coding_is_cool.py
+++
@@ -1,8 +0,0 @@
-#This code is made of two parts, the first part is assigning each word in this sentence "coding is cool" to each variable as shown below.
-#The second part of the code is printing each variable so we can have the sentence "coding is cool" in the output.
-
-word1 = "coding"
-word2 = "is"
-word3 = "cool"
-
-print(word1 +" "+ word2+ " "+ word3)
